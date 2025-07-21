9cba8025cd30f6f878ef5c1295372b153ef3f277
Emma Walsh <walshemma33@outlook.com>
2025-07-21 20:09:00 +0100

    Add history logic

--- src/App.js
+++ src/App.js
@@ -86,17 +86,20 @@ function Board({ xIsNext, squares, onPlay}) {
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0); 
   console.log('history', history);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    // TODO
-    setHistory([...history, nextSquares]); // create new array to contain all items in history
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory); // create new array to contain all items in history
+    setCurrentMove(nextHistory.length - 1); 
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    //TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
